9283da6df9d9542a3218eacb1625de00b1545daa
Bryan Adam B. Tan <barg.tan@gmail.com>
2013-09-04 18:35:42 +0800

    GCP2P draft
    Kailangan pang iimplement yung sa Overlay, at di ko pa magawa yung sa
    processEvent kasi kailangan pa natin idiscuss

--- caviar/gcp2pProtocol.java
+++ caviar/gcp2pProtocol.java
@@ -2,7 +2,35 @@ package caviar;
 
 //protocol
 
-public class gcp2pProtocol implements Overlay{
+public class gcp2pProtocol implements Overlay, CDProtocol, EDProtocol{
+
+//cycle chuchu, ewan ko kung gagawin natin, feeling ko hindi
+public void nextCycle( Node node, int pid ){
 
 
+}
+//eto yung magproprocess ng messages
+public void processEvent( Node node, int pid, Object event ) {
+ArrivedMessage aem = (ArrivedMessage)event;
+
+
+
+}
+
+
+}
+//eto yung class natin for message
+class ArrivedMessage {
+
+	final int    msgType;
+	final Node   sender;
+	final int    chunkNo;
+	final int    chunkSize;
+	public ArrivedMessage(int typeOfMsg, Node sender, int chunkNo, int chunkSize)
+	{
+		this.typeOfMsg    = msgType;
+		this.sender       = sender;
+		this.stripeId     = chunkNo;
+		this.msgParameter = chunkSize;
+	}
 }
