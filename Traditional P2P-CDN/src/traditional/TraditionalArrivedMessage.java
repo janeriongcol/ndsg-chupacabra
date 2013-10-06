812d187ae48b456a1f53215c339ad26be5c30859
Bryan Adam B. Tan <barg.tan@gmail.com>
2013-10-06 18:39:46 +0800

    Here

--- Traditional P2P-CDN/src/traditional/TraditionalArrivedMessage.java
+++ Traditional P2P-CDN/src/traditional/TraditionalArrivedMessage.java
@@ -28,6 +28,7 @@ public class TraditionalArrivedMessage {
 	final Node   sender;
 	public int	 data;
 	public int   data2;
+	public int 	 data3;
 	public Node	 nodeList[];
 	public Node	 node;
 	
@@ -47,6 +48,13 @@ public class TraditionalArrivedMessage {
 		this.sender = sender;
 		this.data = data;
 		this.data2 = data2;
+	}
+	public TraditionalArrivedMessage(int msgType, Node sender, int data, int data2, int data3){
+		this.msgType = msgType;
+		this.sender = sender;
+		this.data = data;
+		this.data2 = data2;
+		this.data3 = data3;
 	}
 	public TraditionalArrivedMessage(int msgType, Node sender, Node[] nodeList){
 		this.msgType = msgType;
